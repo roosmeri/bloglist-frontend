05091924b4d8cf79606ead815da7b22a49849fd8
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-23 19:52:57 +0200

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({blog}) => {
-    return (<div>{blog.title} {blog.author}</div>)
+const Blog = ({ blog }) => {
+    const [showAll, setShowAll] = useState(false)
+
+    const toggleView = () => {
+        setShowAll(!showAll)
+    }
+
+    const shortView = () => {
+        return (<div>{blog.title} {blog.author}</div>)
+    }
+
+    const allView = () => {
+        return (
+            <div>
+                <div>{blog.title} {blog.author}</div>
+                <div>{blog.url}</div>
+                <div>{blog.likes}<button>like</button></div>
+            </div>
+        )
+    }
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+    return (<div style={blogStyle}>
+        {showAll ?
+            allView()
+            :
+            shortView()
+        }
+        {showAll ?
+            <button onClick={toggleView}>hide</button>
+            :
+            <button onClick={toggleView}>view</button>
+        }
+    </div>)
 
 }
 
