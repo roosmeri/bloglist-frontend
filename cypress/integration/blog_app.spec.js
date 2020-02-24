14386bf25e9e034f53d088e77f3afcef9150c4a9
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-24 15:40:20 +0200

    5.18

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,11 +1,34 @@
 describe('Blog app', function () {
-  beforeEach(function() {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
     cy.visit('http://localhost:3000')
+    cy.request('POST', 'http://localhost:3001/api/users',{username:'user1',name:'Roosa',password:'superstrong'})
   })
 
   it('Login form is shown', function () {
     cy.visit('http://localhost:3000/api/blogs')
     cy.contains('Login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.contains('login').click()
+      cy.get('#username').type('user1')
+      cy.get('#password').type('superstrong')
+      cy.get('#login-button').click()
+
+      cy.contains('Roosa logged in')
+
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.contains('login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Incorrect credentials')
+
+    })
   })
 })
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,31 +3,33 @@ import React from 'react'
 
 const LoginForm = ({ handleLogin, username, password, handleUsernameChange,
   handlePasswordChange }) => (
-  <div>
-    <h2>Login</h2>
-    <form onSubmit={handleLogin}>
-      <div>
-        username
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
         <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={handleUsernameChange}
-        />
-      </div>
-      <div>
-        password
+            id='username'
+            type="text"
+            value={username}
+            name="Username"
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          password
         <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={handlePasswordChange}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  </div>
-)
+            id='password'
+            type="password"
+            value={password}
+            name="Password"
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button id='login-button' type="submit">login</button>
+      </form>
+    </div>
+  )
 
 
 export default LoginForm
